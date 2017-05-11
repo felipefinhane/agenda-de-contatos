1f98db7acc5801120414484c6ed8900341e27ae6
Felipe <felipefinhane@gmail.com>
2017-05-11 00:13:18 -0300

    Criando activity do formulário

--- app/src/main/java/com/finhane/agenda/FormularioActivity.java
+++ app/src/main/java/com/finhane/agenda/FormularioActivity.java
@@ -2,12 +2,33 @@ package com.finhane.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
+
+    EditText txtNome, txtEndereco, txtTelefone, txtSite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        Button btSalvarFormulario = (Button) findViewById(R.id.formulario_btSalvarFormulario);
+        txtNome = (EditText) findViewById(R.id.formulario_txtNome);
+        txtEndereco = (EditText) findViewById(R.id.formulario_txtEndereco);
+        txtTelefone = (EditText) findViewById(R.id.formulario_txtTelefone);
+        txtSite = (EditText) findViewById(R.id.formulario_txtSite);
+        RatingBar nota = (RatingBar) findViewById(R.id.formulario_nota);
+
+        btSalvarFormulario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this, "Botão clicado, nome: " + txtNome.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
